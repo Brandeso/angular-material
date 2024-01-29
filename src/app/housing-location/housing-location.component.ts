2e4b7dc32ff81da8f1dea1452886141dfbc0fe3f
Bruno Amezcua <amezcua_bruno@hotmail.com>
2024-01-28 23:50:45 -0600

    #DEV - Added Interface, Property Binding and Services

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
-  styleUrl: './housing-location.component.css'
+  styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
